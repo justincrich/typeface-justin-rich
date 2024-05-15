ca6876e124f2c54d8bf151da3b59f8417bc5669b
Justin Rich <justinrich2008@gmail.com>
2024-05-15 10:43:55 -0600

    base functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,198 @@
-import Image from "next/image";
+"use client";
+import React from "react";
+import * as dateFns from "date-fns";
+import { v4 as uuid } from "uuid";
+
+const DEFAULT_USER_ID = "123";
+
+const SYSTEM_USER_ID = "456";
+
+const USERS: Record<string, { name: string }> = {
+  [DEFAULT_USER_ID]: {
+    name: "Justin",
+  },
+  [SYSTEM_USER_ID]: {
+    name: "System",
+  },
+};
+
+type Message = {
+  id: string;
+  text: string;
+  userId: string;
+  timestamp: number;
+};
+
+type State = {
+  draft: string;
+  messages: Message[];
+};
+
+const initialState = {
+  draft: "",
+  messages: [
+    {
+      id: uuid(),
+      userId: SYSTEM_USER_ID,
+      text: "Hello, how are you?",
+      timestamp: Date.now(),
+    },
+  ],
+};
+
+type ActionType<T extends string, P = any> = P extends undefined
+  ? {
+      type: T;
+    }
+  : {
+      type: T;
+      payload: P;
+    };
+
+type Action =
+  | ActionType<"setDraft", string>
+  | ActionType<"send">
+  | ActionType<"delete", { messageId: string }>;
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "setDraft": {
+      return {
+        ...state,
+        draft: action.payload,
+      };
+    }
+    case "send": {
+      const nextMessages = [
+        ...state.messages,
+        {
+          id: uuid(),
+          text: state.draft,
+          userId: DEFAULT_USER_ID,
+          timestamp: Date.now(),
+        },
+      ];
+      return {
+        ...state,
+        draft: "",
+        messages: nextMessages,
+      };
+    }
+    case "delete": {
+      const nextMessages = [...state.messages];
+      const messageIdx = nextMessages.findIndex(
+        (m) => m.id === action.payload.messageId
+      );
+      if (messageIdx === -1) {
+        return {
+          ...state,
+        };
+      }
+      const message = nextMessages[messageIdx];
+      if (message?.userId !== DEFAULT_USER_ID) return { ...state };
+      nextMessages.splice(messageIdx, 1);
+      return {
+        ...state,
+        messages: nextMessages,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
 
 export default function Home() {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex h-screen flex-col items-center justify-start p-10 font-sans">
+      <div className="flex flex-col items-start justify-center w-full max-w-[700px] h-full">
+        <div className="flex flex-col gap-4 w-full h-full overflow-auto items-start align-top px-8">
+          {state.messages.map((message) => {
+            const isUser = message.userId === DEFAULT_USER_ID;
+            return (
+              <div
+                key={message.id}
+                className={`flex flex-col max-w-3/4 relative  ${
+                  isUser ? `self-end` : `self-start`
+                }`}
+              >
+                {USERS[message.userId] && (
+                  <span
+                    className={`text-slate-500 text-xs ${
+                      isUser ? `self-end` : `self-start`
+                    }`}
+                  >
+                    {USERS[message.userId].name}
+                  </span>
+                )}
+                <div
+                  className={`${
+                    isUser ? "w-full bg-slate-800" : "bg-slate-500"
+                  } p-4 rounded-lg w-full`}
+                >
+                  <p className="text-slate-200">{message.text}</p>
+                </div>
+                {isUser && (
+                  <button
+                    className="absolute -right-[20px] top-1/2 translate-y-[-50%]"
+                    onClick={() =>
+                      dispatch({
+                        type: "delete",
+                        payload: {
+                          messageId: message.id,
+                        },
+                      })
+                    }
+                  >
+                    ❌
+                  </button>
+                )}
+                <span
+                  className={`text-slate-500 text-xs ${
+                    isUser ? `self-end` : `self-start`
+                  }`}
+                >
+                  {dateFns.isToday(new Date(message.timestamp))
+                    ? `Today at ${dateFns.format(
+                        new Date(message.timestamp),
+                        "HH:mm"
+                      )}`
+                    : dateFns.format(
+                        new Date(message.timestamp),
+                        "MM/dd/yyyy HH:mm"
+                      )}
+                </span>
+              </div>
+            );
+          })}
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="flex flex-row border-2 border-slate-800 p-4 rounded-lg w-full self-end">
+          <input
+            className="bg-transparent w-full outline-none"
+            type="text"
+            placeholder="Input message"
+            value={state.draft}
+            onChange={(e) =>
+              dispatch({ type: "setDraft", payload: e.target.value })
+            }
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && state.draft) {
+                e.preventDefault();
+                dispatch({ type: "send" });
+              }
+            }}
+          />
+          <button
+            onClick={() => dispatch({ type: "send" })}
+            disabled={!state.draft}
+            className={
+              !state.draft ? `opacity-50 cursor-not-allowed` : `cursor-pointer`
+            }
+          >
+            Send
+          </button>
+        </div>
       </div>
     </main>
   );
